686d438d3bedd0095e47d4b60d789f3228f5abb6
Yan Lucas Martins <30724634+yanmartins@users.noreply.github.com>
2019-07-03 20:45:29 -0300

    Update DTMF.h

--- simuladorDTMF/DTMF.h
+++ simuladorDTMF/DTMF.h
@@ -11,21 +11,6 @@
 
 class DTMF{
 public:
-	enum DTMF_t{
-		DIGITO_0 		 = 0,
-		DIGITO_1 		 = 1,
-		DIGITO_2 		 = 2,
-		DIGITO_3 		 = 3,
-		DIGITO_4		 = 4,
-		DIGITO_5		 = 5,
-		DIGITO_6 		 = 6,
-		DIGITO_7 		 = 7,
-		DIGITO_8		 = 8,
-		DIGITO_9 		 = 9,
-		DIGITO_asterisco = 10,
-		DIGITO_cerca	 = 11
-
-	};
 	DTMF(GPIO * c1, GPIO * c2, GPIO * c3, GPIO * c4, GPIO *l1,
 			GPIO * l2, GPIO * l3, GPIO * l4);
 	~DTMF(){};
